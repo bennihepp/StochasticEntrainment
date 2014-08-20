bf1de82a160412ea4009df9489068a19831d6edf
Benjamin Hepp <benjamin.hepp@bsse.ethz.ch>
2014-08-20 15:58:48 +0200

    Fixed job array bug

--- VanDerPol/VanDerPol_ArnoldTongue_Stochastic_JobArray.sh
+++ VanDerPol/VanDerPol_ArnoldTongue_Stochastic_JobArray.sh
@@ -3,8 +3,8 @@
 module load matlab
 
 index=$1
-output_prefix=$2
+output=$2
 
-echo "index=$index, output_prefix=$output_prefix"
+echo "index=$index, output=$output"
 
-matlab -nodisplay -singleCompThread -r "VanDerPol_ArnoldTongue_Stochastic_JobArray($index, $output_prefix)"
+matlab -nodisplay -singleCompThread -r "VanDerPol_ArnoldTongue_Stochastic_JobArray($index, $output)"
