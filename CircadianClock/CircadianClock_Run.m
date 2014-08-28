fa74f350160ca026aa6cd4733473613a8db904f2
Benjamin Hepp <benjamin.hepp@bsse.ethz.ch>
2014-08-28 15:06:51 +0200

    updated circadian clock full arnold tongue job array

--- CircadianClock/CircadianClock_Run.m
+++ CircadianClock/CircadianClock_Run.m
@@ -5,7 +5,12 @@ function [T, output] = CircadianClock_Run(Ntrials, t0, tf, dt, recordStep, volum
 
     input_frequency = 1 ./ input_period;
 
+    do_parallel = false;
     if Ntrials > 1
+        do_parallel = true;
+    end
+
+    if do_parallel
         [T, X, ~] = SolveS_Java_Parallel(x0, tf, dt, volume, ...
             input_offset, input_amplitude, input_frequency, Ntrials, ...
             recordStep);
