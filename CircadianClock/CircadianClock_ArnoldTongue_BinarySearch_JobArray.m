e6af542541d974cfc401f9c919cffbb6d5e36227
Benjamin Hepp <benjamin.hepp@bsse.ethz.ch>
2014-08-29 15:48:15 +0200

    update for circadian clock

--- CircadianClock/CircadianClock_ArnoldTongue_BinarySearch_JobArray.m
+++ CircadianClock/CircadianClock_ArnoldTongue_BinarySearch_JobArray.m
@@ -1,7 +1,7 @@
 % brutus
 % POPULATION_AVERAGE=false
 % POPULATION_AVERAGE=true
-% bsub -n 1 -R "rusage[mem=2048]" -W 12:00 -J "job[1-25]" -o logs/CircadianClock_ArnoldTongue_BinarySearch_JobArray_%I.out bash CircadianClock_ArnoldTongue_BinarySearch_JobArray.sh "\$LSB_JOBINDEX" output/ $POPULATION_AVERAGE
+% bsub -n 1 -R "rusage[mem=2048]" -W 16:00 -J "job[1-97]" -o logs2/CircadianClock_ArnoldTongue_BinarySearch_JobArray_%I.out bash CircadianClock_ArnoldTongue_BinarySearch_JobArray.sh "\$LSB_JOBINDEX" output2/ $POPULATION_AVERAGE
 % bsub -n 1 -R "rusage[mem=1536]" -W 8:00 -J "job_comb" -o logs/CircadianClock_ArnoldTongue_BinarySearch_JobArray_Combine.out bash CircadianClock_ArnoldTongue_BinarySearch_JobArray.sh 0 output/ $POPULATION_AVERAGE
 %
 % INDEX=71; bsub -n 1 -R "rusage[mem=2048]" -W 16:00 -o logs/CircadianClock_ArnoldTongue_BinarySearch_JobArray_$INDEX.out bash CircadianClock_ArnoldTongue_BinarySearch_JobArray.sh $INDEX output/ $POPULATION_AVERAGE
