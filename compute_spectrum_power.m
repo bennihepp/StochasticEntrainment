0d77d579ceeea8b589c811107fe64c25576b4925
Benjamin Hepp <benjamin.hepp@bsse.ethz.ch>
2014-08-31 16:37:02 +0200

    Updated NFkB model

--- compute_spectrum_power.m
+++ compute_spectrum_power.m
@@ -3,7 +3,7 @@ function power = compute_spectrum_power(omega, y, om, dom)
     i1 = find(omega >= om - dom, 1, 'first');
     i2 = find(omega <= om + dom, 1, 'last');
 
-    if ~isempty(i1) || isempty(i2)
+    if isempty(i1) || isempty(i2)
 
         power = 0;
 
