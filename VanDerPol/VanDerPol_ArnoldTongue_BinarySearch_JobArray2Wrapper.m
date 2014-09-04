07a9b404320d219684a6006ec37b0238a438632c
Benjamin Hepp <benjamin.hepp@bsse.ethz.ch>
2014-09-04 13:27:54 +0200

    Update to VanDerPol job array

--- VanDerPol/VanDerPol_ArnoldTongue_BinarySearch_JobArray2Wrapper.m
+++ VanDerPol/VanDerPol_ArnoldTongue_BinarySearch_JobArray2Wrapper.m
@@ -10,21 +10,21 @@ function VanDerPol_ArnoldTongue_BinarySearch_JobArray2Wrapper(n, filename_prefix
     end
 
 	try
-		CircadianClock_ArnoldTongue_BinarySearch_JobArray2(n, filename_prefix, input_periods, input_amplitude_tolerance, Ntrials, population_average);
+		VanDerPol_ArnoldTongue_BinarySearch_JobArray2(n, filename_prefix, input_periods, input_amplitude_tolerance, Ntrials, population_average);
 	catch e
 		display('pause');
 		pause(5);
 		try
-			CircadianClock_ArnoldTongue_BinarySearch_JobArray2(n, filename_prefix, input_periods, input_amplitude_tolerance, Ntrials, population_average);
+			VanDerPol_ArnoldTongue_BinarySearch_JobArray2(n, filename_prefix, input_periods, input_amplitude_tolerance, Ntrials, population_average);
 		catch e
 			display('pause');
 			pause(5);
 			try
-				CircadianClock_ArnoldTongue_BinarySearch_JobArray2(n, filename_prefix, input_periods, input_amplitude_tolerance, Ntrials, population_average);
+				VanDerPol_ArnoldTongue_BinarySearch_JobArray2(n, filename_prefix, input_periods, input_amplitude_tolerance, Ntrials, population_average);
 			catch e
 				display('pause');
 				pause(5);
-				CircadianClock_ArnoldTongue_BinarySearch_JobArray2(n, filename_prefix, input_periods, input_amplitude_tolerance, Ntrials, population_average);
+				VanDerPol_ArnoldTongue_BinarySearch_JobArray2(n, filename_prefix, input_periods, input_amplitude_tolerance, Ntrials, population_average);
 			end
 		end
 	end
