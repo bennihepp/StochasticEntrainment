c615fa87e6505dacb44e7d5ae594b2daf57169bf
Benjamin Hepp <benjamin.hepp@bsse.ethz.ch>
2014-09-19 18:03:07 +0200

    changes to job arrays

--- VanDerPol/VanDerPol_ArnoldTongue_BinarySearch_JobArray3.sh
+++ VanDerPol/VanDerPol_ArnoldTongue_BinarySearch_JobArray3.sh
@@ -12,4 +12,4 @@ entrainment_threshold=$7
 
 echo "index=$index, output=$output, input_periods=$input_periods, tolerance=$tolerance, Ntrials=$Ntrials, population_average=$population_average, entrainment_threshold=$entrainment_threshold"
 
-matlab -nodisplay -singleCompThread -r "VanDerPol_ArnoldTongue_BinarySearch_JobArray2Wrapper($index, '$output', $input_periods, $tolerance, $Ntrials, $population_average, $$entrainment_threshold); exit;"
+matlab -nodisplay -singleCompThread -r "VanDerPol_ArnoldTongue_BinarySearch_JobArray2Wrapper($index, '$output', $input_periods, $tolerance, $Ntrials, $population_average, $entrainment_threshold); exit;"
